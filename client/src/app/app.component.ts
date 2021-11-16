ec43c104bdbaccb3d62bea05425913e2f8bd00d5
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-16 17:07:59 +0100

    Étape 10: Utiliser une requête HTTP pour récupérer des données
    On récupère la liste des super-héros depuis une URL "locale" (le fichier
    JSON asserts/superheroes.json).

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Bienvenue dans le TP Angular ENVOL 2021';
-  superheroes: string[] = ['Hulk', 'Spider-Man', 'Thor', 'Iron Man'];
+  superheroes: string[] = [];
   newHero: string = "Black Linux !";
   newHeroAdded: boolean = false;
   // Service HttpClient
@@ -16,6 +16,14 @@ export class AppComponent {
 
   constructor(httpClient: HttpClient) {
     this.httpClient = httpClient;
+  }
+
+  // Fonction d'initialisation (lancée une seule fois par Angular lorsque le composant est connecté)
+  ngOnInit(): void {
+    // Récupère la liste des super-héros et la stocke dans superheroes
+    this.httpClient.get<string[]>('assets/superheroes.json').subscribe(data => {
+      this.superheroes = data;
+    });
   }
 
   // Ajoute newHero si il n'est pas dans la liste et désactive le bouton
