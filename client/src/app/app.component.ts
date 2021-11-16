f03aeebf2cf8262923cd12a42182b880f3ed8de8
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-16 15:46:01 +0100

    Étape 8: Passer la liste de super héros en Input
    Les fonctionnalités d'affichage sont déplacées dans SuperHeroesListComponent.
    Ce composant accepte une liste de héros en entrée.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,17 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title: string = 'client';
-    superheroes: string[] = [
-        'Hulk', 'Spider-Man', 'Thor', 'Iron Man'
-    ];
-    newHero: string = "Black Linux !";
-    newHeroAdded: boolean = false;
-
-    // Ajoute newHero si il n'est pas dans la liste et désactive le bouton
-    addNewHero(): void {
-        if (! this.newHeroAdded ) {
-            this.superheroes.push(this.newHero);
-            this.newHeroAdded = true;
-        }
-    }
 }
--- client/src/app/super-heroes-list/super-heroes-list.component.ts
+++ client/src/app/super-heroes-list/super-heroes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-super-heroes-list',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./super-heroes-list.component.scss']
 })
 export class SuperHeroesListComponent implements OnInit {
+
+  @Input() superheroes: string[] = [];
+  newHero: string = "Black Linux !";
+  newHeroAdded: boolean = false;
+
+  // Ajoute newHero si il n'est pas dans la liste et désactive le bouton
+  addNewHero(): void {
+    if (! this.newHeroAdded ) {
+      this.superheroes.push(this.newHero);
+      this.newHeroAdded = true;
+    }
+  }
 
   constructor() { }
 
